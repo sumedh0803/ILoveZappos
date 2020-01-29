f431f7c6b9fe816a4620d595cadf7fc013828619
sumedh <sumedh0803@gmail.com>
2020-01-29 10:42:46 -0600

    Completed bids table

---
+++ app/src/main/java/com/example/ilovezappos/Utils/BidsItem.java
@@ -0,0 +1,25 @@
+package com.example.ilovezappos.Utils;
+
+import android.widget.TextView;
+
+public class BidsItem {
+    private String bids, value,amt;
+
+    public BidsItem(String bids, String value, String amt) {
+        this.bids = bids;
+        this.value = value;
+        this.amt = amt;
+    }
+
+    public String getBids() {
+        return bids;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public String getAmt() {
+        return amt;
+    }
+}
