820ea46277a079b93833c59c1e6b493a8819ad8c
sumedh <sumedh0803@gmail.com>
2020-01-28 18:41:59 -0600

    Initial Commit

---
+++ app/src/main/java/com/example/ilovezappos/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.ilovezappos;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.ilovezappos.Fragments.AsksFragment;
+import com.example.ilovezappos.Fragments.BidsFragment;
+import com.example.ilovezappos.Fragments.TransactionFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new TransactionFragment()).commit();
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomnav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFrag = null;
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.txn: selectedFrag = new TransactionFragment();
+                            Toast.makeText(getApplicationContext(),"TXN",Toast.LENGTH_LONG).show(); break;
+                        case R.id.asks: selectedFrag = new AsksFragment();Toast.makeText(getApplicationContext(),"ASKS",Toast.LENGTH_LONG).show(); break;
+                        case R.id.bids: selectedFrag = new BidsFragment();Toast.makeText(getApplicationContext(),"BIDS",Toast.LENGTH_LONG).show(); break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFrag).commit();
+                    return true;
+                }
+            };
+}
