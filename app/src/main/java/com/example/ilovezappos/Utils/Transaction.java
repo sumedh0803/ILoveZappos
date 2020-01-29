9967785c445355bf70a15e821b77d2c5304b4e47
sumedh <sumedh0803@gmail.com>
2020-01-28 23:19:54 -0600

    Completed graph and notifications

---
+++ app/src/main/java/com/example/ilovezappos/Utils/Transaction.java
@@ -0,0 +1,36 @@
+package com.example.ilovezappos.Utils;
+
+import com.github.mikephil.charting.data.Entry;
+
+import java.util.Comparator;
+
+public class Transaction{
+    String date,price,type;
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    /*Comparator for sorting the list by Student Name*/
+    public static Comparator<Entry> timeComparator = new Comparator<Entry>() {
+
+        public int compare(Entry e1, Entry e2) {
+            long time1 =(long) e1.getX();
+            long time2 =(long) e2.getX();
+
+            //ascending order
+            return String.valueOf(time1).compareTo(String.valueOf(time2));
+
+            //descending order
+            //return StudentName2.compareTo(StudentName1);
+        }};
+
+}
