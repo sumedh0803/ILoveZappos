0daf1271516ae4115362ab1d960e4bb70e6a1409
sumedh <sumedh0803@gmail.com>
2020-01-30 16:52:31 -0600

    Added Splash Screen

---
+++ app/src/main/java/com/example/ilovezappos/SplashScreen.java
@@ -0,0 +1,45 @@
+package com.example.ilovezappos;
+/**
+ * Written By: Sumedh Sen
+ * Date: 12/26/2019
+ * */
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
+        getSupportActionBar().hide(); //hide the title bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN); //show the activity in full screen
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+    @Override
+    public void run() {
+        Intent i = new Intent(SplashScreen.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+},3000);
+
+
+    }
+}
