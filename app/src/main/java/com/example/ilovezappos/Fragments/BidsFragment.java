aa31b15df5a9095dfc0244f57f25c86e45313601
sumedh <sumedh0803@gmail.com>
2020-01-30 15:57:41 -0600

    Added swipe to refresh on bids fragment

--- app/src/main/java/com/example/ilovezappos/Fragments/BidsFragment.java
+++ app/src/main/java/com/example/ilovezappos/Fragments/BidsFragment.java
@@ -1,21 +1,26 @@
 package com.example.ilovezappos.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LayoutAnimationController;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.example.ilovezappos.API.BitstampBidsApi;
 import com.example.ilovezappos.R;
+import com.example.ilovezappos.Utils.AsksItem;
 import com.example.ilovezappos.Utils.Bids;
 import com.example.ilovezappos.Utils.BidsAdapter;
 import com.example.ilovezappos.Utils.BidsItem;
@@ -34,11 +39,31 @@ public class BidsFragment extends Fragment {
     private RecyclerView.Adapter adapter;
     private  RecyclerView.LayoutManager layoutManager;
     private TextView timestamp;
+    private SwipeRefreshLayout swipeRefreshLayout;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_bids,container,false);
         getActivity().setTitle("Bids");
+        bindViews(view);
+        loadUiElements();
+        swipeRefreshLayout.setColorSchemeColors(getResources().getColor(R.color.colorAccent));
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadUiElements();
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                },2500);
+            }
+        });
+        return view;
+    }
+
+    private void loadUiElements() {
         Retrofit rf = new Retrofit.Builder()
                 .baseUrl("https://www.bitstamp.net/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -49,37 +74,45 @@ public class BidsFragment extends Fragment {
             @Override
             public void onResponse(Call<Bids> call, Response<Bids> response) {
 
-                ArrayList<BidsItem> bidsData = new ArrayList<>();
-                for(List<String> bid : response.body().getBids())
-                {
-                    Float temp = (Float.parseFloat(bid.get(0))*Float.parseFloat(bid.get(1)));
-                    String tempS = temp.toString();
-                    bidsData.add(new BidsItem(bid.get(0),bid.get(1),tempS));
-                    //System.out.println(bid.get(0)+":"+bid.get(1));
-                }
-                recyclerView = view.findViewById(R.id.recycler);
-                timestamp = view.findViewById(R.id.timestamp);
-                layoutManager = new LinearLayoutManager(getContext());
-                adapter = new BidsAdapter(bidsData,getContext());
-                recyclerView.setLayoutManager(layoutManager);
-                recyclerView.setAdapter(adapter);
-                LayoutAnimationController animationController = AnimationUtils.loadLayoutAnimation(getContext(),R.anim.layout_animation);
-                recyclerView.setLayoutAnimation(animationController);
-                recyclerView.getAdapter().notifyDataSetChanged();
-                recyclerView.scheduleLayoutAnimation();
+                ArrayList<BidsItem> bidsData;
+                bidsData = generateBidsData(response);
+                prepareRecyclerView(bidsData,getContext());
 
-                String timest = response.body().getTimestamp();
-                java.util.Date time=new java.util.Date((Long.parseLong(timest)*1000));
+                java.util.Date time=new java.util.Date((Long.parseLong(response.body().getTimestamp())*1000));
                 timestamp.setText(time.toString());
-
-
             }
-
             @Override
             public void onFailure(Call<Bids> call, Throwable t) {
-                System.out.println(t.getMessage());
+                Toast.makeText(getContext(),"ERROR: "+t.getMessage(),Toast.LENGTH_LONG).show();
             }
         });
-        return view;
+    }
+
+    private void prepareRecyclerView(ArrayList<BidsItem> bidsData, Context context) {
+        layoutManager = new LinearLayoutManager(context);
+        adapter = new BidsAdapter(bidsData,context);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+        LayoutAnimationController animationController = AnimationUtils.loadLayoutAnimation(getContext(),R.anim.layout_animation);
+        recyclerView.setLayoutAnimation(animationController);
+        recyclerView.getAdapter().notifyDataSetChanged();
+        recyclerView.scheduleLayoutAnimation();
+    }
+
+    private ArrayList<BidsItem> generateBidsData(Response<Bids> response) {
+        ArrayList<BidsItem> bidsDataLocal = new ArrayList<>();
+        for(List<String> bid : response.body().getBids())
+        {
+            Float temp = (Float.parseFloat(bid.get(0))*Float.parseFloat(bid.get(1)));
+            String tempS = temp.toString();
+            bidsDataLocal.add(new BidsItem(bid.get(0),bid.get(1),tempS));
+        }
+        return bidsDataLocal;
+    }
+
+    private void bindViews(View view) {
+        recyclerView = view.findViewById(R.id.recycler);
+        timestamp = view.findViewById(R.id.timestamp);
+        swipeRefreshLayout = view.findViewById(R.id.swipe);
     }
 }
